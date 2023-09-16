18ae26540e677cd680a5573e8b79378acb741cb6
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-16 17:14:05 +0200

    fix the last card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,7 +15,6 @@ import { useData } from '../../contexts/DataContext';
 const Page = () => {
   const { data } = useData();
   const sortedEvents = data ? [...data.events].sort((evtA, evtB) => (new Date(evtA.date) < new Date(evtB.date) ? -1 : 1)) : [];
-
   const last = sortedEvents ? sortedEvents[sortedEvents.length - 1] : null;
   return (
     <>
